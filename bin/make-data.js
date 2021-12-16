bf9af15045d590eacf28953c84ea98654e0e3e14
Nicholas Cloud <nicholas.cloud@protonmail.com>
2021-12-16 13:36:28 -0600

    no region modification at runtime

--- bin/make-data.js
+++ bin/make-data.js
@@ -1,9 +1,12 @@
 const axios = require( "axios" );
 const fs = require( "fs" );
 const path = require( "path" );
+const uniq = require( "lodash/uniq" );
+const addedRegions = require( "./added-regions.json" );
+const renamedRegions = require( "./renamed-regions.json" );
 
 const PROJECT_ROOT = path.resolve( __dirname, ".." );
-const DATA_ROOT = path.join( PROJECT_ROOT, "server", "data", "regions" );
+const DATA_ROOT = path.join( PROJECT_ROOT, "server", "data" );
 
 const f3MapURL = process.env.F3_MAP_URL;
 if ( !f3MapURL ) {
@@ -16,13 +19,11 @@ async function getMapData() {
 	return response.data;
 }
 
-const uniq = ( arr ) => {
-	const map = {};
-	for ( const item of arr ) {
-		map[ item ] = null;
-	}
-	return Object.keys( map );
+const renameRegions = ( region ) => {
+	return renamedRegions[ region ] || region;
 };
+
+const trim = str => str.trim();
 
 ( async function () {
 	/*
@@ -46,7 +47,7 @@ const uniq = ( arr ) => {
 	],
 	*/
 
-	console.info("fetching data...");
+	console.info( "fetching data..." );
 	const pointsJSON = await getMapData();
 
 	const values = pointsJSON.values.slice( 1 );
@@ -62,14 +63,16 @@ const uniq = ( arr ) => {
 
 	allAOs.NONE = [ "NONE" ];
 
-	console.info("writing AOs...");
+	console.info( "writing AOs..." );
 	fs.writeFile( path.join( DATA_ROOT, "aos.json" ), JSON.stringify( allAOs, null, 2 ), ( err ) => {
 		if ( err ) {
 			console.error( err );
 			return process.exit( 1 );
 		}
-		console.info("writing regions...");
-		fs.writeFile( path.join( DATA_ROOT, "regions.json" ), JSON.stringify( Object.keys( allAOs ).sort() ), ( err ) => {
+		const regions = Object.keys( allAOs );
+		const adjustedRegions = uniq( regions.concat( addedRegions ).map( renameRegions ) ).map( trim ).sort();
+		console.info( "writing regions..." );
+		fs.writeFile( path.join( DATA_ROOT, "regions.json" ), JSON.stringify( adjustedRegions ), ( err ) => {
 			if ( err ) {
 				console.error( err );
 				return process.exit( 1 );
